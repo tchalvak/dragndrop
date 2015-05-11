5d313c0719e609f0ff8927d1256fb6ffa25d450a
Roy Ronalds <tchalvakspam@gmail.com>
2015-05-11 09:22:56 -0400

    Drag: Allow dragging of the hubs to the tiers.

--- js/dragndrop.js
+++ js/dragndrop.js
@@ -11,3 +11,22 @@ function drop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
 }
+
+$(function(){
+	// Prevent default on drop targets
+	// Once drag starts, set the data.
+	// On drop transfer the data
+  $(".tier").on("dragover",function(e){
+    e.preventDefault();
+  });
+  $(".hub-area button").on("dragstart",function(e){
+    e.originalEvent.dataTransfer.setData("text",e.target.id);
+  });
+  $(".tier").on("drop",function(e){
+    e.preventDefault();
+    // Get the id of the draggable element, must have an id
+    var data=e.originalEvent.dataTransfer.getData("text");
+    console.log('data', data, 'target', e.target);
+    e.target.appendChild(document.getElementById(data));
+  });
+});
