b2931f1e5a8e56d326a7c8bccd7f58ffb9c849ec
Roy Ronalds <tchalvakspam@gmail.com>
2015-05-11 10:16:40 -0400

    Drag: Better styling, add save button (which does nothing for now), and reindenting of the javascript.

--- js/dragndrop.js
+++ js/dragndrop.js
@@ -12,24 +12,54 @@ function drop(ev) {
     ev.target.appendChild(document.getElementById(data));
 }
 
+// Take the hubs in each tier and capture their data and save them.
+function saveHubs(){
+	// Eventually, this will push to the REST api, but for now, we'll just output the data.
+	data = {};
+	$('.tier').each(function(){
+	});
+
+	return data;
+}
+
+// A quick and dirty function to render the save data
+function renderSaveData(data){
+	// Just output the data as a list into the info area.
+
+}
+
 $(function(){
 	// Prevent default on drop targets
+	$(".tier").on("dragover",function(e){
+	e.preventDefault();
+	});
+
 	// Once drag starts, set the data.
-	// On drop transfer the data
-  $(".tier").on("dragover",function(e){
-    e.preventDefault();
-  });
-  $(".hub-area button").on("dragstart",function(e){
-  	// Set the hubId for transferring.
-    e.originalEvent.dataTransfer.setData("text",e.target.id);
-  });
-  $(".tier").on("drop",function(e){
-    e.preventDefault();
-    // Get the id of the draggable element, must have an id
-    var hubId=e.originalEvent.dataTransfer.getData("text");
-    console.log('data', hubId, 'target', e.target);
-    var clone = $('#'+hubId).clone().attr("id", hubId+'-copy');
-    console.log('clone:', clone, 'typeof clone:', typeof(clone));
-    e.target.appendChild(clone.get(0));
-  });
+	$(".hub-area button").on("dragstart",function(e){
+		// Set the hubId for transferring.
+		e.originalEvent.dataTransfer.setData("text",e.target.id);
+	});
+
+	var copyId = 1;
+
+	// On drop transfer the data, clone an element, and then place it in the new location.
+	$(".tier").on("drop",function(e){
+		e.preventDefault();
+		// Get the id of the draggable element, must have an id
+		var hubId=e.originalEvent.dataTransfer.getData("text");
+		console.log('data', hubId, 'target', e.target);
+		// Create a clone with new data, but referring to the original.
+		var clone = $('#'+hubId).clone().attr("id", hubId+'-copy-'+copyId).data('original-id', hubId);
+		copyId++; // Increment the clone ids so they're unique.
+		console.log('clone:', clone, 'typeof clone:', typeof(clone));
+		e.target.appendChild(clone.get(0));
+	});
+
+
+	$('#save-hubs').hide().submit(function(e){
+		e.preventDefault();
+		var data = saveHubs();
+		renderSaveData(data);
+		return false;
+	});
 });
